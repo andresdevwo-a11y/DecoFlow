69e4a5a4f9b2d52914762a4806c630d08896dd54
andresdevwo-stack <andresdevwo@gmail.com>
2026-02-05 16:30:42 -0500

    Update CreateSectionModal.js

--- components/CreateSectionModal.js
+++ components/CreateSectionModal.js
@@ -8,7 +8,6 @@ const COLORS_LIST = [
     '#EF4444', // Red
     '#F59E0B', // Amber
     '#10B981', // Emerald
-    '#3B82F6', // Blue
     '#6366F1', // Indigo
     '#8B5CF6', // Violet
     '#EC4899', // Pink
